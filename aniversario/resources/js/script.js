7a025f1e6cd6ca17a6359c570477983e66139b47
Luís Gustavo <lgustavogdc@gmail.com>
2020-03-17 20:10:13 -0300

    First commit

---
+++ aniversario/resources/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.videos_div').slick({
+        autoplay: true,
+        autoplaySpeed: 60000,
+        dots: true,
+        fade:true,
+        focusOnSelect:true,
+        draggable: false
+      });
+  });
